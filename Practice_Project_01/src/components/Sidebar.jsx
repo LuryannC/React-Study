f0dcbdad205ff6991ca6f5d1a98932b5ae752ab4
Luryann A. Cervi <92046725+LuryannC@users.noreply.github.com>
2024-09-30 11:46:01 +0100

    Finished practice project

--- Practice_Project_01/src/components/Sidebar.jsx
+++ Practice_Project_01/src/components/Sidebar.jsx
@@ -8,15 +8,42 @@ import {
 import { createPortal } from "react-dom";
 import Button from "./Button.jsx";
 
-export default function Sidebar({ projects, onStartAddProject, ...props }) {
+export default function Sidebar({
+  projects,
+  onStartAddProject,
+  onSelectProject,
+  selectedProjectId,
+  ...props
+}) {
   const hasProjects = Object.keys(projects).length > 0;
 
   return (
     <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
-      <h2 className="font-bold text-stone-200 uppercase mb-8 md:text-xl">Your Projects</h2>
+      <h2 className="font-bold text-stone-200 uppercase mb-8 md:text-xl">
+        Your Projects
+      </h2>
       <div>
         <Button onClick={onStartAddProject}>+ Add Project</Button>
       </div>
+      <ul className="mt-8">
+        {projects.map((project) => {
+          let cssClasses = "w-full text-left px-2 py-1 rounded-sm my-1 hover:text-stone-200 hover:bg-stone-800";
+
+          if(project.id === selectedProjectId){
+            cssClasses += ' bg-stone-800 text-stone-200';
+          }
+          else
+          {
+            cssClasses += ' hover:bg-stone-400';
+          }
+
+          return (<li key={project.id}>
+            <button onClick={() => onSelectProject(project.id)} className={cssClasses}>
+              {project.title}
+            </button>
+          </li>);
+        })}
+      </ul>
     </aside>
   );
 }
